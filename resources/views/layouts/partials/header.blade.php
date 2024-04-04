83b23dc920ecfc83d5f45df4ba577cf31b2784b7
davidemarchini <marchini73@gmail.com>
2024-04-04 18:12:36 +0200

    post

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-      <a class="navbar-brand" href="#">Laravel project</a>
+      <a class="navbar-brand" href="#">Portfolio Davide</a>
       <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
         class="navbar-toggler" data-bs-target="#navbarSupportedContent" data-bs-toggle="collapse" type="button">
         <span class="navbar-toggler-icon"></span>
